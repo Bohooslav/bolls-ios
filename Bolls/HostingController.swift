9296f81374d72935083f65a5286f2ffae92f252b
Bohooslav <bpavlisinec@gmail.com>
2020-08-23 12:32:04 +0300

    Load the app Handle first load in offline Handle in app links Handle OAuth

---
+++ Bolls/HostingController.swift
@@ -0,0 +1,27 @@
+//
+//  HostingController.swift
+//  Bolls
+//
+//  Created by Богуслав Павлишинець on 19.08.2020.
+//  Copyright © 2020 Bohuslav Pavlyshynets. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+class HostingController: UIHostingController<ContentView> {
+//    override var prefersStatusBarHidden: Bool {
+//        return true
+//    }
+//    func setStatusBar(backgroundColor: UIColor) {
+//        let statusBarFrame: CGRect
+//        if #available(iOS 13.0, *) {
+//            statusBarFrame = view.window?.windowScene?.statusBarManager?.statusBarFrame ?? CGRect.zero
+//        } else {
+//            statusBarFrame = UIApplication.shared.statusBarFrame
+//        }
+//        let statusBarView = UIView(frame: statusBarFrame)
+//        statusBarView.backgroundColor = backgroundColor
+//        view.addSubview(statusBarView)
+//    }
+}
---
+++ Bolls/statusBarChange.swift
@@ -0,0 +1,25 @@
+//
+//  statusBarChange.swift
+//  Bolls
+//
+//  Created by Богуслав Павлишинець on 19.08.2020.
+//  Copyright © 2020 Bohuslav Pavlyshynets. All rights reserved.
+//
+
+import Foundation
+//
+//extension UINavigationController {
+//
+//    func setStatusBar(backgroundColor: UIColor) {
+//        let statusBarFrame: CGRect
+//        if #available(iOS 13.0, *) {
+//            statusBarFrame = view.window?.windowScene?.statusBarManager?.statusBarFrame ?? CGRect.zero
+//        } else {
+//            statusBarFrame = UIApplication.shared.statusBarFrame
+//        }
+//        let statusBarView = UIView(frame: statusBarFrame)
+//        statusBarView.backgroundColor = backgroundColor
+//        view.addSubview(statusBarView)
+//    }
+//
+//}
